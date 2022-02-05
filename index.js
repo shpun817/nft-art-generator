2ff048577f1e9da7952403bf062bc65770324150
Sanford Pun <punsanford@gmail.com>
2022-02-06 00:13:13 +0800

    Remove names

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const { readFileSync, writeFileSync, readdirSync, rmSync, existsSync, mkdirSync } = require('fs');
 const sharp = require('sharp');
 
+// Input
+const numNfts = 1000;
+const outputSize = 256;
+
 const template = `
-    <svg width="256" height="256" viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="${outputSize}" height="${outputSize}" viewBox="0 0 ${outputSize} ${outputSize}" fill="none" xmlns="http://www.w3.org/2000/svg">
         <!-- bg -->
         <!-- head -->
         <!-- hair -->
@@ -11,42 +15,17 @@ const template = `
         <!-- mouth -->
         <!-- beard -->
     </svg>
-`
-
-const takenNames = new Set();
+`;
 const takenFaces = new Set();
-const numNfts = 1000;
 let idx = numNfts;
 
 function randInt(max) {
     return Math.floor(Math.random() * (max + 1));
 }
 
-function randElement(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-}
-
-
-function getRandomName() {
-    const adjectives = 'fired trashy tubular nasty jacked swol buff ferocious firey flamin agnostic artificial bloody crazy cringey crusty dirty eccentric glutinous harry juicy simple stylish awesome creepy corny freaky shady sketchy lame sloppy hot intrepid juxtaposed killer ludicrous mangy pastey ragin rusty rockin sinful shameful stupid sterile ugly vascular wild young old zealous flamboyant super sly shifty trippy fried injured depressed anxious clinical'.split(' ');
-    const names = 'aaron bart chad dale earl fred grady harry ivan jeff joe kyle lester steve tanner lucifer todd mitch hunter mike arnold norbert olaf plop quinten randy saul balzac tevin jack ulysses vince will xavier yusuf zack roger raheem rex dustin seth bronson dennis'.split(' ');
-    
-    const randAdj = randElement(adjectives);
-    const randName = randElement(names);
-    const name =  `${randAdj}-${randName}`;
-
-
-    if (takenNames.has(name) || !name) {
-        return getRandomName();
-    } else {
-        takenNames.add(name);
-        return name;
-    }
-}
-
 function getLayer(name, skip=0.0) {
     const svg = readFileSync(`./layers/${name}.svg`, 'utf-8');
-    const re = /(?<=\<svg\s*[^>]*>)([\s\S]*?)(?=\<\/svg\>)/g
+    const re = /(?<=\<svg\s*[^>]*>)([\s\S]*?)(?=\<\/svg\>)/g;
     const layer = svg.match(re)[0];
     return Math.random() > skip ? layer : '';
 }
@@ -69,15 +48,12 @@ function createImage(idx) {
     const nose = randInt(4); 
     const mouth = randInt(5);
     const beard = randInt(3);
-    // 18,900 combinations
 
     const face = [hair, eyes, mouth, nose, beard].join('');
 
     if (takenFaces.has(face)) {
         createImage();
     } else {
-        const name = getRandomName()
-        console.log(name);
         takenFaces.add(face);
 
         const final = template
@@ -90,8 +66,8 @@ function createImage(idx) {
             .replace('<!-- beard -->', getLayer(`beard${beard}`, 0.5))
 
         const meta = {
-            name,
-            description: `A drawing of ${name.split('-').join(' ')}`,
+            name: `Person ${idx}`,
+            description: `A drawing of Person ${idx}`,
             image: `${idx}.png`,
             attributes: [
                 { 
@@ -99,7 +75,7 @@ function createImage(idx) {
                     rarity: 0.5
                 }
             ]
-        }
+        };
         writeFileSync(`./out/${idx}.json`, JSON.stringify(meta))
         writeFileSync(`./out/${idx}.svg`, final)
         svgToPng(idx)
