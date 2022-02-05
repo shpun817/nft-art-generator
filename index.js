98c917a57e7df089fc02f347d89c9adf04c8d838
Sanford Pun <punsanford@gmail.com>
2022-02-06 01:39:31 +0800

    Fixed enumeration of generation

--- index.js
+++ index.js
@@ -2,31 +2,64 @@ const { readFileSync, writeFileSync, readdirSync, rmSync, existsSync, mkdirSync 
 const sharp = require('sharp');
 
 // Input
-const numNfts = 1000;
+const desiredNumNfts = 1000;
 const outputSize = 256;
+const traits = [
+    { name: "bg", numVariants: 5 },
+    { name: "head", numVariants: 1 },
+    { name: "hair", numVariants: 7 },
+    { name: "eyes", numVariants: 9 },
+    { name: "nose", numVariants: 4 },
+    { name: "mouth", numVariants: 5 },
+    { name: "beard", numVariants: 3 },
+];
+// End of Input
+
+const numNfts = Math.min(
+    desiredNumNfts,
+    traits.reduce((n, {numVariants}) => {return n * numVariants}, 1)
+);
 
 const template = `
     <svg width="${outputSize}" height="${outputSize}" viewBox="0 0 ${outputSize} ${outputSize}" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <!-- bg -->
-        <!-- head -->
-        <!-- hair -->
-        <!-- eyes -->
-        <!-- nose -->
-        <!-- mouth -->
-        <!-- beard -->
+    ${
+        traits.reduce((str, trait) => {
+            str += `<!-- ${trait.name} -->`;
+            return str;
+        }, "")
+    }
     </svg>
 `;
+
 const takenFaces = new Set();
 
-function randInt(max) {
-    return Math.floor(Math.random() * (max + 1));
+// Excluding current value
+const cumulativeNumVariants = traits.map((_, i, arr) => {
+    let value = 1;
+    for (let j = 0; j < i; ++j) {
+        value *= arr[j].numVariants;
+    }
+    return value;
+});
+
+function quotient(x, divisor) {
+    return Math.trunc(x / divisor);
 }
 
-function getLayer(name, skip=0.0) {
+function idxToFace(idx) {
+    const face = [];
+
+    for (let i = 0; i < traits.length; ++i) {
+        face.push(quotient(idx, cumulativeNumVariants[i]) % traits[i].numVariants);
+    }
+
+    return {face, faceString: face.join('-')};
+}
+
+function getLayer(name) {
     const svg = readFileSync(`./layers/${name}.svg`, 'utf-8');
     const re = /(?<=\<svg\s*[^>]*>)([\s\S]*?)(?=\<\/svg\>)/g;
-    const layer = svg.match(re)[0];
-    return Math.random() > skip ? layer : '';
+    return svg.match(re)[0];
 }
 
 async function svgToPng(name) {
@@ -41,39 +74,27 @@ async function svgToPng(name) {
 
 function createImage(idx) {
 
-    const bg = randInt(5);
-    const hair = randInt(7);
-    const eyes = randInt(9);
-    const nose = randInt(4); 
-    const mouth = randInt(5);
-    const beard = randInt(3);
+    const {face, faceString} = idxToFace(idx);
+    const [bg, head, hair, eyes, nose, mouth, beard] = face;
 
-    const face = [hair, eyes, mouth, nose, beard].join('');
-
-    if (takenFaces.has(face)) {
+    if (takenFaces.has(faceString)) {
         createImage();
     } else {
-        takenFaces.add(face);
+        takenFaces.add(faceString);
 
         const final = template
             .replace('<!-- bg -->', getLayer(`bg${bg}`))
-            .replace('<!-- head -->', getLayer('head0'))
+            .replace('<!-- head -->', getLayer(`head${head}`))
             .replace('<!-- hair -->', getLayer(`hair${hair}`))
             .replace('<!-- eyes -->', getLayer(`eyes${eyes}`))
             .replace('<!-- nose -->', getLayer(`nose${nose}`))
             .replace('<!-- mouth -->', getLayer(`mouth${mouth}`))
-            .replace('<!-- beard -->', getLayer(`beard${beard}`, 0.5))
+            .replace('<!-- beard -->', getLayer(`beard${beard}`))
 
         const meta = {
             name: `Person ${idx}`,
             description: `A drawing of Person ${idx}`,
             image: `${idx}.png`,
-            attributes: [
-                { 
-                    beard: '',
-                    rarity: 0.5
-                }
-            ]
         };
         writeFileSync(`./out/${idx}.json`, JSON.stringify(meta))
         writeFileSync(`./out/${idx}.svg`, final)
